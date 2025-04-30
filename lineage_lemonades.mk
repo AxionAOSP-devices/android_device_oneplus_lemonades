33135f03207f33713ae7c962006b101362fe0bc5
Manidweep <manidweep678@gmail.com>
2025-04-30 01:10:46 -0400

    lemonades: Axion Bringup

--- lineage_lemonades.mk
+++ lineage_lemonades.mk
@@ -30,3 +30,35 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=OnePlus9R \
     SystemDevice=OnePlus9R \
     SystemName=OnePlus9R
+
+# Device Flags
+
+# Maintainer name (use "_" for spaces, e.g., "rmp_22" → "rmp 22" in UI)
+AXION_MAINTAINER := Itachi
+
+# Processor name (use "_" for spaces)
+AXION_PROCESSOR := Snapdragon_870
+
+# Define rear camera specs (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 48,16,5,2
+
+# Define front camera specs
+AXION_CAMERA_FRONT_INFO := 16
+
+# Torch Control
+TORCH_STR_SUPPORTED := true
+
+# Los prebuilts
+TARGET_INCLUDES_LOS_PREBUILTS := true
+
+# Ax FX
+TARGET_INCLUDE_AXFX := true
+
+# BLur
+TARGET_ENABLE_BLUR := true
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= true
+
+# Path for charge toggle
+BYPASS_CHARGE_TOGGLE_PATH ?= /sys/class/oplus_chg/battery/mmi_charging_enable
